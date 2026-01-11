c6f77600f8212c65c3108bbc5a0019538c92509e
yukimatsuno <yukimatsuno@users.noreply.github.com>
2026-01-11 14:18:40 +0900

    first design css

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from bson import ObjectId
-
 from app.db import collection
-
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
